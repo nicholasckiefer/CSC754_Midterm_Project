1823bbb369d5449ffbe90d72d903610347c5794b
Eric Benedetto <ericsplace3@gmail.com>
2021-11-06 17:04:56 -0400

    stats

--- Source.cpp
+++ Source.cpp
@@ -9,6 +9,32 @@
 #include "Event.h"
 #include "Vehicle.h"
 
+float standardDeviation(std::vector<float> vals, float mean) {
+	float sum = 0;
+
+	for (float& val : vals) {
+		sum += pow((val - mean), 2);
+	}
+
+	return float(sqrt((float(1) / float(vals.size())) * float(sum)));
+}
+
+float calcMean(std::vector<float> vals) {
+	float sum = 0;
+	
+	for (float& val : vals) {
+		sum += val;
+	}
+
+	return float(sum / vals.size());
+}
+
+std::tuple<float, float> confidenceInterval(std::vector<float> vals, float z, float mean, float sd) {
+	float n = z * (sd / sqrt(vals.size()));
+
+	return std::make_tuple(mean - n, mean + n);
+}
+
 int main()
 {
 	// Simulation Parameters
@@ -28,8 +54,14 @@ int main()
 	float totalCarWashBusyTime = 0;
 	float totalTruckWashBusyTime = 0;
 	float totalMaxCarWaitTime = 0;
+	std::vector<float> totalAvgCarWashUtilizations;
+	std::vector<float> totalAvgTruckWashUtilizations;
 	std::vector<float> totalAvgCarWaitTimes;
 	std::vector<float> totalAvgTruckWaitTimes;
+	std::vector<float> totalMinCarWaitTimes;
+	std::vector<float> totalMaxCarWaitTimes;
+	std::vector<float> totalMinTruckWaitTimes;
+	std::vector<float> totalMaxTruckWaitTimes;
 	float totalMinTruckWaitTime = 0;
 	float totalMaxTruckWaitTime = 0;
 
@@ -46,14 +78,14 @@ int main()
 		int arrivals = 0;
 		int timeSpentWaiting = 0; // Track time customer spent waiting
 		int vehiclesOverMinWait = 0;
-		float minCarWaitTime = 0;
-		float maxCarWaitTime = 0;
+		float minCarWaitTime = std::numeric_limits<float>::max();
+		float maxCarWaitTime = std::numeric_limits<float>::min();
 		std::vector<float> carWaitTimes;
 		std::vector<float> truckWaitTimes;
 		int carsWaitingOverThreshold = 0;
 		int truckWaitingOverThreshold = 0;
-		float minTruckWaitTime = 0;
-		float maxTruckWaitTime = 0;
+		float minTruckWaitTime = std::numeric_limits<float>::max();
+		float maxTruckWaitTime = std::numeric_limits<float>::min();
 		std::vector<Vehicle*> carCustomers;
 		std::vector<Vehicle*> truckCustomers;
 
@@ -164,17 +196,25 @@ int main()
 			case EventType::VEHICLE_WASH:
 			{
 				std::cout << t << ": " << customer->getVehicleTypeString() << " " << customer->getID() << " Is Being Washed" << std::endl;
+				
+				timeSpentWaiting = t - customer->getID();
 
 				if (customer->isCar()) {
 					carWashLastBusy = t;
+					if (timeSpentWaiting < minCarWaitTime) minCarWaitTime = timeSpentWaiting;
+					if (timeSpentWaiting > maxCarWaitTime) maxCarWaitTime = timeSpentWaiting;
+					carWaitTimes.push_back(timeSpentWaiting);
 				}
 				else {
+					if (timeSpentWaiting < minTruckWaitTime) minTruckWaitTime = timeSpentWaiting;
+					if (timeSpentWaiting > maxTruckWaitTime) maxTruckWaitTime = timeSpentWaiting;
+
 					truckWashLastBusy = t;
+					truckWaitTimes.push_back(timeSpentWaiting);
 				}
 
 				customer->arriveAtWasher(t);
 				
-				timeSpentWaiting = t - customer->getID();
 				if (timeSpentWaiting > 1)
 				{
 					vehiclesOverMinWait++;
@@ -208,6 +248,17 @@ int main()
 
 		std::cout << std::endl;
 
+		float avgCarWait = calcMean(carWaitTimes);
+		totalAvgCarWaitTimes.push_back(avgCarWait);
+
+		float avgTruckWait = calcMean(truckWaitTimes);
+		totalAvgTruckWaitTimes.push_back(avgTruckWait);
+
+		totalMinCarWaitTimes.push_back(minCarWaitTime);
+		totalMaxCarWaitTimes.push_back(maxCarWaitTime);
+		totalMinTruckWaitTimes.push_back(minCarWaitTime);
+		totalMaxTruckWaitTimes.push_back(maxTruckWaitTime);
+
 		totalTime += t;
 		totalArrivals += arrivals;
 		totalCarsSeen += totalCars;
@@ -215,8 +266,10 @@ int main()
 		totalCarsServiced += carsServiced;
 		totalTrucksServiced += trucksServiced;
 		totalVehiclesTurnedAway += vehiclesTurnedAway;
+		totalAvgCarWashUtilizations.push_back(carWashBusyTime / t);
 		totalCarWashBusyTime += carWashBusyTime;
 		totalTruckWashBusyTime += truckWashBusyTime;
+		totalAvgTruckWashUtilizations.push_back(truckWashBusyTime / t);
 		totalVehiclesOverMinWait += vehiclesOverMinWait;
 
 		std::cout << "Total Arrivals: " << arrivals << std::endl;
@@ -225,27 +278,112 @@ int main()
 		std::cout << "Cars Serviced: " << carsServiced << std::endl;
 		std::cout << "Trucks Serviced: " << trucksServiced << std::endl;
 		std::cout << "Vehicles Turned Away: " << vehiclesTurnedAway << std::endl;
-		std::cout << "Percentage of vehicles with wait time > 1 min: " << ((float)(vehiclesOverMinWait) / (float)(totalCars + totalTrucks)) * 100 << "%" << std::endl;
+		std::cout << "Average Car Wait Time: " << avgCarWait << " minutes" << std::endl;
+		std::cout << "Average Truck Wait Time: " << avgTruckWait << " minutes" << std::endl;
+		std::cout << "Percentage of Vehicles with Wait Time > " << waitTimeThreshold << " Minutes: " << ((float)(vehiclesOverMinWait) / (float)(totalCars + totalTrucks)) * 100 << " % " << std::endl;
 	}
 
 	std::cout << std::endl;
+	std::cout << "RESULTS" << std::endl;
+	std::cout << "######################################" << std::endl;
 	
+	float totalAvgCarWait = calcMean(totalAvgCarWaitTimes);
+	float totalAvgTruckWait = calcMean(totalAvgTruckWaitTimes);
+
+	float totalAvgMinCarWait = calcMean(totalMinCarWaitTimes);
+	float totalAvgMaxCarWait = calcMean(totalMaxCarWaitTimes);
+	float totalAvgMinTruckWait = calcMean(totalMinTruckWaitTimes);
+	float totalAvgMaxTruckWait = calcMean(totalMaxTruckWaitTimes);
+
+	float totalAvgCarWashUtilization = calcMean(totalAvgCarWashUtilizations);
+	float totalAvgTruckWashUtilization = calcMean(totalAvgTruckWashUtilizations);
+
+	float carWaitDeviation = standardDeviation(totalAvgCarWaitTimes, totalAvgCarWait);
+	float truckWaitDeviation = standardDeviation(totalAvgTruckWaitTimes, totalAvgTruckWait);
+
+	float carMinWaitDeviation = standardDeviation(totalMinCarWaitTimes, totalAvgMinCarWait);
+	float carMaxWaitDeviation = standardDeviation(totalMaxCarWaitTimes, totalAvgMaxCarWait);
+	float truckMinWaitDeviation = standardDeviation(totalMinTruckWaitTimes, totalAvgMinTruckWait);
+	float truckMaxWaitDeviation = standardDeviation(totalMaxTruckWaitTimes, totalAvgMaxTruckWait);
+
+	float carUtilizationDeviation = standardDeviation(totalAvgCarWashUtilizations, totalAvgCarWashUtilization);
+	float truckUtilizationDeviation = standardDeviation(totalAvgTruckWashUtilizations, totalAvgTruckWashUtilization);
+
+	std::tuple<float, float> carWaitConfidence = confidenceInterval(totalAvgCarWaitTimes, 1.960, totalAvgCarWait, carWaitDeviation);
+	std::tuple<float, float> truckWaitConfidence = confidenceInterval(totalAvgTruckWaitTimes, 1.960,totalAvgTruckWait, truckWaitDeviation);
+
+	std::tuple<float, float> carMinWaitConfidence = confidenceInterval(totalMinCarWaitTimes, 1.960, totalAvgMinCarWait, carMinWaitDeviation);
+	std::tuple<float, float> carMaxWaitConfidence = confidenceInterval(totalMaxCarWaitTimes, 1.960, totalAvgMaxCarWait, carMaxWaitDeviation);
+	std::tuple<float, float> truckMinWaitConfidence = confidenceInterval(totalMinTruckWaitTimes, 1.960, totalAvgMinTruckWait, truckMinWaitDeviation);
+	std::tuple<float, float> truckMaxWaitConfidence = confidenceInterval(totalMaxTruckWaitTimes, 1.960, totalAvgMaxTruckWait, truckMaxWaitDeviation);
+
+	std::tuple<float, float> carUtilizationConfidence = confidenceInterval(totalAvgCarWashUtilizations, 1.960, totalAvgCarWashUtilization, carUtilizationDeviation);
+	std::tuple<float, float> truckUtilizationConfidence = confidenceInterval(totalAvgTruckWashUtilizations, 1.960, totalAvgTruckWashUtilization, truckUtilizationDeviation);
+
 	std::cout << "Overall Time: " << totalTime << " minutes" << std::endl;
 	std::cout << "Average Time: " << float(totalTime / numRuns) << " minutes" << std::endl;
+	std::cout << std::endl;
+	
 	std::cout << "Overall Total Vehicles: " << totalArrivals << " vehicles" << std::endl;
 	std::cout << "Overall Total Cars: " << totalCarsSeen << " cars" << std::endl;
 	std::cout << "Overall Total Trucks: " << totalTrucksSeen << " trucks" << std::endl;
+	std::cout << std::endl;
+
 	std::cout << "Overall Cars Serviced: " << totalCarsServiced << " cars" << std::endl;
 	std::cout << "Overall Trucks Serviced: " << totalTrucksServiced << " trucks" << std::endl;
+	std::cout << std::endl;
+	
 	std::cout << "Overall Vehicles Turned Away: " << totalVehiclesTurnedAway << " vehicles" << std::endl;
 	std::cout << "Percentage Vehicles Turned Away: " << float(totalVehiclesTurnedAway / float(totalCarsSeen + totalTrucksSeen)) * 100 << "%" << std::endl;
+	std::cout << std::endl;
+
 	std::cout << "Overall CarWash Busy Time: " << totalCarWashBusyTime << " minutes" << std::endl;
 	std::cout << "Average CarWash Busy Time: " << float(totalCarWashBusyTime / numRuns) << " minutes" << std::endl;
+	std::cout << std::endl;
+
 	std::cout << "Average CarWash Utilization: " << float(totalCarWashBusyTime / totalTime) * 100 << "%" << std::endl;
+	std::cout << std::endl;
+
 	std::cout << "Overall TruckWash Busy Time: " << totalTruckWashBusyTime << " minutes" << std::endl;
-	std::cout << "Average CarWash Busy Time: " << float(totalTruckWashBusyTime / numRuns) << " minutes" << std::endl;
+	std::cout << "Average TruckWash Busy Time: " << float(totalTruckWashBusyTime / numRuns) << " minutes" << std::endl;
+	std::cout << std::endl;
+
 	std::cout << "Average TruckWash Utilization : " << float(totalTruckWashBusyTime / totalTime) * 100 << "%" << std::endl;
-	std::cout << "Percentage of vehicles with wait time > 1 min: " << ((float)(totalVehiclesOverMinWait) / (float)(totalCarsSeen + totalTrucksSeen)) * 100 << "%" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Average Car Wait Time: " << totalAvgCarWait << " minutes" << std::endl;
+	std::cout << "Average Truck Wait Time: " << totalAvgTruckWait  << " minutes" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Percentage of Vehicles with Wait Time > " << waitTimeThreshold << " Minutes: " << ((float)(totalVehiclesOverMinWait) / (float)(totalCarsSeen + totalTrucksSeen)) * 100 << "%" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "CarWash Utilization Standard Deviation: " << carUtilizationDeviation * 100 << "%" << std::endl;
+	std::cout << "TruckWash Utilization Standard Deviation: " << truckUtilizationDeviation * 100 << "%" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Car Wait Time Standard Deviation: " << carWaitDeviation << " minutes" << std::endl;
+	std::cout << "Car Min Wait Time Standard Deviation: " << carMinWaitDeviation << " minutes" << std::endl;
+	std::cout << "Car Max Wait Time Standard Deviation: " << carMaxWaitDeviation << " minutes" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Truck Wait Time Standard Deviation: " << truckWaitDeviation << " minutes" << std::endl;
+	std::cout << "Truck Min Wait Time Standard Deviation: " << truckMinWaitDeviation << " minutes" << std::endl;
+	std::cout << "Truck Max Wait Time Standard Deviation: " << truckMaxWaitDeviation << " minutes" << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "CarWash Utilization Confidence Interval: " << std::get<0>(carUtilizationConfidence) * 100 << " - " << std::get<1>(carUtilizationConfidence) * 100 << "%" << std::endl;
+	std::cout << "TruckWash Utilization Confidence Interval: " << std::get<0>(truckUtilizationConfidence) * 100 << " - " << std::get<1>(truckUtilizationConfidence) * 100 << "%" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Car Wait Time Confidence Interval: " << std::get<0>(carWaitConfidence) << " - " << std::get<1>(carWaitConfidence) << " minutes" << std::endl;
+	std::cout << "Car Min Wait Time Confidence Interval: " << std::get<0>(carMinWaitConfidence) << " - " << std::get<1>(carWaitConfidence) << " minutes" << std::endl;
+	std::cout << "Car Max Wait Time Confidence Interval: " << std::get<0>(carMaxWaitConfidence) << " - " << std::get<1>(carWaitConfidence) << " minutes" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Truck Wait Time Confidence Interval: " << std::get<0>(truckWaitConfidence) << " - " << std::get<1>(truckWaitConfidence) << " minutes" << std::endl;
+	std::cout << "Truck Min Wait Time Confidence Interval: " << std::get<0>(truckMinWaitConfidence) << " - " << std::get<1>(truckWaitConfidence) << " minutes" << std::endl;
+	std::cout << "Truck Max Wait Time Confidence Interval: " << std::get<0>(truckMaxWaitConfidence) << " - " << std::get<1>(truckWaitConfidence) << " minutes" << std::endl;
 
 	std::system("pause");
 	return 0;
